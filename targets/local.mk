03a556cbae7bfdc26021102dd9a79e439c2c4259
Ahmed Jazzar <me@ahmedjazzar.com>
2019-10-30 23:41:55 -0700

    More dynamic host records

--- targets/local.mk
+++ targets/local.mk
@@ -1,14 +1,14 @@
 
 environment.debug:  ## Prints the values of the environemnt variables to be used in the make command as define in .env.* files.
 	@echo -e "${cyan}ALLOW_FIREWALL${normal}        $(ALLOW_FIREWALL)"
-	@echo -e "${cyan}SHELL_OUTPUT${normal}        $(SHELL_OUTPUT)"
 	@echo -e "${cyan}DENY_FIREWALL${normal}         $(DENY_FIREWALL)"
 	@echo -e "${cyan}DEVSTACK_REPO_BRANCH${normal}  $(DEVSTACK_REPO_BRANCH)"
 	@echo -e "${cyan}DEVSTACK_REPO_URL${normal}     $(DEVSTACK_REPO_URL)"
 	@echo -e "${cyan}DEVSTACK_RUN_COMMAND${normal}  $(DEVSTACK_RUN_COMMAND)"
 	@echo -e "${cyan}DEVSTACK_WORK_DIR${normal}     $(DEVSTACK_WORK_DIR)"
 	@echo -e "${cyan}DISK_SIZE${normal}             $(DISK_SIZE)"
-	@echo -e "${cyan}HOST_NAME${normal}             $(HOST_NAME)"
+	@echo -e "${cyan}EDX_HOST_NAMES{normal}         $(EDX_HOST_NAMES)"
+	@echo -e "${cyan}HOST_NAMES{normal}             $(HOST_NAMES)"
 	@echo -e "${cyan}HOSTS_FILE${normal}            $(HOSTS_FILE)"
 	@echo -e "${cyan}IMAGE_FAMILY${normal}          $(IMAGE_FAMILY)"
 	@echo -e "${cyan}IMAGE_NAME${normal}            $(IMAGE_NAME)"
@@ -21,15 +21,15 @@ environment.debug:  ## Prints the values of the environemnt variables to be used
 	@echo -e "${cyan}RESTRICT_INSTANCE${normal}     $(RESTRICT_INSTANCE)"
 	@echo -e "${cyan}SERVICE_ACCOUNT_EMAIL${normal} $(SERVICE_ACCOUNT_EMAIL)"
 	@echo -e "${cyan}SERVICE_KEY_PATH${normal}      $(SERVICE_KEY_PATH)"
+	@echo -e "${cyan}SHELL_OUTPUT${normal}          $(SHELL_OUTPUT)"
 	@echo -e "${cyan}SSH_KEY${normal}               $(SSH_KEY)"
-	@echo -e "${cyan}TAHOE_HOST_NAME${normal}       $(TAHOE_HOST_NAME)"
 	@echo -e "${cyan}TMP_DIR${normal}               $(TMP_DIR)"
 	@echo -e "${cyan}USER_NAME${normal}             $(USER_NAME)"
 	@echo -e "${cyan}VERBOSITY${normal}             $(VERBOSITY)"
 	@echo -e "${cyan}ZONE${normal}                  $(ZONE)"
 
 local.ssh.config: ve/bin/ansible-playbook
-	@echo -e "Updating necessary records in SSH related files...    [~/.ssh/config, ~/.ssh/knwon_hosts]"
+	@echo -e "Updating necessary records in SSH related files...    ${dim}[~/.ssh/config, ~/.ssh/knwon_hosts]${normal}"
 	$(eval IP_ADDRESS := $(shell make instance.ip))
 	@. ve/bin/activate; ansible-playbook local.yml \
 		--connection=local \
@@ -58,15 +58,16 @@ local.hosts.update: ve/bin/ansible-playbook  ## Updates your hosts file by addin
 		--connection=local \
 		-i '127.0.0.1,' \
 		--tags hosts_update \
-		-e "IP_ADDRESS=$(IP_ADDRESS) TAHOE_HOST_NAME=$(TAHOE_HOST_NAME)" local.yml > $(SHELL_OUTPUT) || (echo -e "${redbold}ERROR configuring hosts records.${normal}" && make error && exit 1)
+		-e "IP_ADDRESS=$(IP_ADDRESS) EDX_HOST_NAMES=$(EDX_HOST_NAMES)" local.yml > $(SHELL_OUTPUT) || (echo -e "${redbold}ERROR configuring hosts records.${normal}" && make error && exit 1)
 	@echo -e "${green}Your hosts have been configured successfully!${normal}"
 
 local.hosts.revert: ve/bin/ansible-playbook  ## Updates your hosts file by removing the added hosts from it.
-	@echo "Reverting made local changes...    [/etc/hosts, ~/.ssh/config]"
+	@echo -e "Reverting made local changes...    ${dim}[/etc/hosts, ~/.ssh/config]${normal}"
 	@make sudocheck
 	@. ve/bin/activate; sudo ansible-playbook \
 		--connection=local \
 		-i '127.0.0.1,' \
+		-e "EDX_HOST_NAMES=$(EDX_HOST_NAMES)" \
 		--tags hosts_revert local.yml > $(SHELL_OUTPUT) || (echo -e "${redbold}ERROR reverting local changes.${normal}" && make error && exit 1)
 	@echo -e "${green}Your local changes have been reverted successfully!${normal}"
 
