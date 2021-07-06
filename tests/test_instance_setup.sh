95b91f4bc9b343f8daaa587b96db536e4e3de69e
Ahmed Jazzar <11036472+ahmedaljazzar@users.noreply.github.com>
2021-07-06 14:43:19 -0700

    Pull updated branches

--- tests/test_instance_setup.sh
+++ tests/test_instance_setup.sh
@@ -6,13 +6,31 @@ echo "::group::Start the SSH agent"
 eval "$(ssh-agent -s)"
 echo "::endgroup::"
 
-echo "::group::Setting up the instance"
 if [[ "$IMAGE" ]]; then
+  echo "::group::Setting up the instance from $IMAGE"
   ./sultan instance setup --image "$IMAGE"
+  echo "::endgroup::"
+
+  echo "::group::Scanning github.com"
+  ./sultan instance run "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+  echo "::endgroup::"
+
+  echo "::group::Installing requirements"
+  ./sultan devstack make requirements
+  echo "::endgroup::"
+
+  echo "::group::Clone devstack repos"
+  ./sultan devstack make dev.clone
+  echo "::endgroup::"
+
+  echo "::group::Pulling devstack images"
+  ./sultan devstack make dev.pull
+  echo "::endgroup::"
 else
+  echo "::group::Setting up the instance"
   ./sultan instance setup
+  echo "::endgroup::"
 fi
-echo "::endgroup::"
 
 echo "::group::Make sure the instance is pingable"
 ./sultan instance ping
