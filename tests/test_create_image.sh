86b476b7d74cfe73ecb3b23bf8e6a8d210950487
Ahmed Jazzar <11036472+ahmedaljazzar@users.noreply.github.com>
2021-07-09 10:20:00 -0700

    Fix images created from outdated branches

--- tests/test_create_image.sh
+++ tests/test_create_image.sh
@@ -11,6 +11,22 @@ echo "::group::Setup the instance"
 ./sultan instance setup --image "$IMAGE"
 echo "::endgroup::"
 
+echo "::group::Scanning github.com"
+./sultan instance run "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+echo "::endgroup::"
+
+echo "::group::Installing requirements"
+./sultan devstack make requirements
+echo "::endgroup::"
+
+echo "::group::Clone devstack repos"
+./sultan devstack make dev.clone
+echo "::endgroup::"
+
+echo "::group::Pulling devstack images"
+./sultan devstack make dev.pull
+echo "::endgroup::"
+
 
 echo "::group::Create the image"
 ./sultan image create --name "${IMAGE}"
